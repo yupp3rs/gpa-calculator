b78a73c809671f73473e74505eb22e5b0df1f5a8
david <e9968cbe-1991-4252-87ff-87e542351fdd@anonaddy.me>
2021-03-06 20:07:07 -0500

    added bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function() { 
+  var script = document.createElement('script'); 
+  script.src = 'https://cdn.jsdelivr.net/gh/yupp3rs/gpa-calculator/gpa-calculator.js'; 
+  document.body.appendChild(script);
+})();
